5aad7acb9580dc7b131305afd0a0c36b8a80cf68
Don Wibier <don@anykey.nl>
2020-02-26 08:45:46 +0100

    Add project files.

---
+++ TestCafeDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace TestCafeDemo.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+        public EditorsViewModel EditorModel { get; set; } = new EditorsViewModel();
+
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+        public bool ShowForm { get; set; } = true;
+        public void OnPost(EditorsViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ShowForm = false;
+                EditorModel = model;
+            }
+            
+
+        }
+	}
+
+    public class EditorsViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ TestCafeDemo/TestCafe/ui-test.js
@@ -0,0 +1,25 @@
+﻿//.tc01 
+import { Selector } from 'testcafe';
+const dataSet = require('./data.json');
+fixture`Getting started fixture`
+    .page`https://localhost:44374/`;
+
+est('Registration Fixture', async t => {
+    awtait t
+        .typeText('#frmName', 'don')
+        .typeText('#frmEmail', 'donw@devexpress.com')
+        .click('#register')
+
+        .expect(Selector('#response').textContent).eql("Thank you John you will receive an email at donw@devexpress.com");
+});
+
+dataSet.forEach(data => {
+    test(`Processing '${data.name}'`, async t => {
+        await t
+            .typeText('#frmName', data.name)
+            .typeText('#frmEmail', data.email)
+            .click('#register')
+
+            .expect(Selector('#response').textContent).eql(data.resultText);
+        });
+});
